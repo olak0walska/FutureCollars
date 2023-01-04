1148a6e6404dfa09ed1f7dc2a61924e355ff64af
Aleksandra Kowalska <okowalska57@gmail.com>
2023-01-04 17:13:36 +0100

    poprawione

--- src/main/java/org/example/lesson6task1/FileReader.java
+++ src/main/java/org/example/lesson6task1/FileReader.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-public class ReadAndDisplay {
+public class FileReader {
 
     {
         try {
--- src/main/java/org/example/lesson6task1/Main.java
+++ src/main/java/org/example/lesson6task1/Main.java
@@ -2,6 +2,6 @@ package org.example.lesson6task1;
 
 public class Main {
     public static void main(String[] args) {
-        ReadAndDisplay readAndDisplay = new ReadAndDisplay();
+        FileReader readAndDisplay = new FileReader();
     }
 }
