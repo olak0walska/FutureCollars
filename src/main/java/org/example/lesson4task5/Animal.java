cb7112bb86d9a9ae2b6cf0f0a6b494ab18e22ece
Aleksandra Kowalska <okowalska57@gmail.com>
2023-01-04 11:04:50 +0100

    lesson4task5

---
+++ src/main/java/org/example/lesson4task5/Animal.java
@@ -0,0 +1,9 @@
+package org.example.lesson4task5;
+
+public class Animal {
+    String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/org/example/lesson4task5/WildAnimals.java
@@ -0,0 +1,7 @@
+package org.example.lesson4task5;
+
+public class WildAnimals extends Animal {
+    public WildAnimals(String name) {
+        super(name);
+    }
+}
