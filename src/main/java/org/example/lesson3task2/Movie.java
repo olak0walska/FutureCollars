35eda0062fe84168aff63b6d45dc12166bd515a9
Aleksandra Kowalska <okowalska57@gmail.com>
2023-01-04 15:11:36 +0100

    .

--- src/main/java/org/example/lesson3task2/Movie.java
+++ src/main/java/org/example/lesson3task2/Movie.java
@@ -2,9 +2,9 @@ package org.example.lesson3task2;
 
 public class Movie {
     final private String title;
-   final private String nameOfDirector;
-   final private int durationInMinutes;
-   final private int releaseYear;
+    final private String nameOfDirector;
+    final private int durationInMinutes;
+    final private int releaseYear;
 
     public Movie(String title, String nameOfDirector, int durationInMinutes, int releaseYear) {
         this.title = title;
