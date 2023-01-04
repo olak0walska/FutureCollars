36de380f15244ba6a27febc14898c936868a31d4
Aleksandra Kowalska <okowalska57@gmail.com>
2023-01-04 11:20:07 +0100

    lesson6task1

---
+++ src/main/java/org/example/lesson6task1/Main.java
@@ -0,0 +1,11 @@
+package org.example.lesson6task1;
+
+import java.io.BufferedWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Main {
+    public static void main(String[] args) {
+        ReadAndDisplay readAndDisplay = new ReadAndDisplay();
+    }
+}
---
+++ src/main/java/org/example/lesson6task1/ReadAndDisplay.java
@@ -0,0 +1,17 @@
+package org.example.lesson6task1;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class ReadAndDisplay {
+
+    {
+        try {
+            String text = Files.readString(Path.of("C:\\Users\\Acer\\Desktop\\data.txt"));
+            System.out.println(text);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
