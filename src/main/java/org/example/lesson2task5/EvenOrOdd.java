e2bcb74fda72a221d24198f0a732112e5f846a1c
Aleksandra Kowalska <okowalska57@gmail.com>
2023-01-04 09:58:03 +0100

    lesson2task5

---
+++ src/main/java/org/example/lesson2task5/EvenOrOdd.java
@@ -0,0 +1,12 @@
+package org.example.lesson2task5;
+
+public class EvenOrOdd {
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    public static boolean isOdd(int number) {
+        return number % 2 == 1;
+    }
+}
+
