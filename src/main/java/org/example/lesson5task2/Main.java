117e00ffaf879509e92327f6ebda2d11159cf6cf
Aleksandra Kowalska <okowalska57@gmail.com>
2023-01-07 19:53:54 +0100

    poprawione

--- src/main/java/org/example/lesson5task2/Main.java
+++ src/main/java/org/example/lesson5task2/Main.java
@@ -2,24 +2,16 @@ package org.example.lesson5task2;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array = new int[100];
-
-        for (int i = 0; i < 100; i++)
-            array[i] = i + 1;
-
-        for (int i = 0; i < 100; i++) {
-
-            if ((i + 1) % 3 == 0 && ((i + 1) % 5 != 0))
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 != 0) {
                 System.out.println("Fizz");
-
-            if ((i + 1) % 5 == 0 && ((i + 1) % 3 != 0))
+            } else if (i % 5 == 0 && i % 3 != 0) {
                 System.out.println("Buzz");
-
-            if ((i + 1) % 15 == 0)
+            } else if (i % 15 == 0) {
                 System.out.println("FizBuzz");
-
-            if ((i + 1) % 3 != 0 && ((i + 1) % 5 != 0))
-                System.out.println(array[i]);
+            } else {
+                System.out.println(i);
+            }
         }
     }
 }
