be34519a29680ecfbff16c13508757e1cea1e013
Aleksandra Kowalska <okowalska57@gmail.com>
2023-01-13 17:18:33 +0100

    lesson7task1

---
+++ src/test/java/org/example/lesson7task1/EvenTest.java
@@ -0,0 +1,23 @@
+package org.example.lesson7task1;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EvenTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {2, 4, 6, 8, 10})
+    void shouldVerifyNumberIsEven(int input) {
+        Assertions.assertTrue(Even.isEven(input));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {3, 11, 21, 29, 1043})
+    void shouldVerifyNumberIsNotEven(int input) {
+        Assertions.assertFalse(Even.isEven(input));
+    }
+}
