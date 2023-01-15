4e30d189ffcfe68fea7b9d1954515cc519b0d1e2
Aleksandra Kowalska <okowalska57@gmail.com>
2023-01-15 11:37:08 +0100

    lesson7task4

---
+++ src/main/java/org/example/lesson7task4/LengthOfString.java
@@ -0,0 +1,7 @@
+package org.example.lesson7task4;
+
+public class LengthOfString {
+    public static int length(String text){
+        return text.length();
+    }
+}
---
+++ src/test/java/org/example/lesson7task4/LengthOfStringTest.java
@@ -0,0 +1,29 @@
+package org.example.lesson7task4;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringLengthTest {
+
+    @ParameterizedTest
+    @MethodSource
+    void shouldCountLengthOfString(String input, int expected) {
+        int actualValue = input.length();
+        Assertions.assertEquals(expected, actualValue);
+    }
+
+    private static Stream<Arguments> shouldCountLengthOfString() {
+        return Stream.of(
+                Arguments.of("okno", 4),
+                Arguments.of("ola kinga klaudia", 17),
+                Arguments.of("ala ma kota", 11)
+        );
+    }
+}
