e3304bb1852c047a85a71bbbbff638c7907fa8d4
Aleksandra Kowalska <okowalska57@gmail.com>
2023-01-16 20:52:28 +0100

    lesson6task8

---
+++ src/main/java/org/example/lesson6task8/WrongTypeOfDataException.java
@@ -0,0 +1,7 @@
+package org.example.lesson6task8;
+
+public class WrongTypeOfDataException extends RuntimeException {
+    public WrongTypeOfDataException(String message) {
+        super(message);
+    }
+}
