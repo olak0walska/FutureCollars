ac0670d7a4750d07cfc6a79c98b61f7909223537
Aleksandra Kowalska <okowalska57@gmail.com>
2023-01-20 19:53:15 +0100

    ?

---
+++ src/main/java/org/example/lesson3task3/Manager.java
@@ -0,0 +1,26 @@
+package org.example.lesson3task3;
+
+
+public class Manager extends BaseEmployee {
+    private static final int BASE_SALARY = 5000;
+    private static final int BONUS = 500;
+    private int bonus;
+
+    public Manager(String name, String surname, int employmentYears, int salary) {
+        super(name, surname, employmentYears);
+        this.bonus = BONUS;
+
+    }
+
+    public Manager(String name, String surname, int employmentYears) { //(int salary, String name, String surname, int employmentYear
+        super(name, surname, employmentYears, BASE_SALARY);
+        this.bonus = BONUS;
+    }
+
+    @Override
+    public int calculateFullSalary() {
+        return this.getSalary() + bonus;
+    }
+}
+
+
