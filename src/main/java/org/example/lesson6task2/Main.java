2065d557938c2ec8b113a976ad4bb99a4e8de8bc
Aleksandra Kowalska <okowalska57@gmail.com>
2023-01-21 19:37:05 +0100

    lesson6task2

---
+++ src/main/java/org/example/lesson6task2/Main.java
@@ -0,0 +1,7 @@
+package org.example.lesson6task2;
+
+public class Main {
+    public static void main(String[] args) {
+        EverySecondLine.ReadAndDisplay("C:\\Users\\Acer\\Desktop\\data.txt");
+    }
+}
