36285b1829ea33ad703e98ab5ee91db4a532f765
Aleksandra Kowalska <okowalska57@gmail.com>
2023-01-24 20:45:45 +0100

    .

--- src/main/java/org/example/lesson9task5/BubbleSort.java
+++ src/main/java/org/example/lesson9task5/BubbleSort.java
@@ -7,9 +7,11 @@ public class BubbleSort<T extends Comparable<T>> {
     public void sort(List<T> list) {
         for (int i = 0; i < list.size() - 1; i++) {
             for (int j = 0; j < list.size() - i - 1; j++) {
-                T temp = list.get(j);
-                list.set(j, list.get(j + 1));
-                list.set(j + 1, temp);
+                if (list.get(j).compareTo(list.get(j + 1)) > 0) {
+                    T temp = list.get(j);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, temp);
+                }
             }
         }
     }
