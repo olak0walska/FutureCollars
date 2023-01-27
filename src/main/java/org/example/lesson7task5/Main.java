d8f9f674ba940fd84da33520317f8c23765abcc8
Aleksandra Kowalska <okowalska57@gmail.com>
2023-01-27 08:32:48 +0100

    .

--- src/main/java/org/example/lesson7task5/Main.java
+++ src/main/java/org/example/lesson7task5/Main.java
@@ -4,10 +4,18 @@ import java.time.Month;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(SeasonDuration.getSeason(Month.JANUARY));
-        System.out.println(SeasonDuration.getSeason(Month.APRIL));
-        System.out.println(SeasonDuration.getSeason(Month.JULY));
-        System.out.println(SeasonDuration.getSeason(Month.SEPTEMBER));
-        System.out.println(SeasonDuration.getSeason(null));
+        SeasonDuration seasonDuration = new SeasonDuration();
+        System.out.println(seasonDuration.getSeason(Month.JANUARY));
+        System.out.println(seasonDuration.getSeason(Month.FEBRUARY));
+        System.out.println(seasonDuration.getSeason(Month.MARCH));
+        System.out.println(seasonDuration.getSeason(Month.APRIL));
+        System.out.println(seasonDuration.getSeason(Month.MAY));
+        System.out.println(seasonDuration.getSeason(Month.JUNE));
+        System.out.println(seasonDuration.getSeason(Month.JULY));
+        System.out.println(seasonDuration.getSeason(Month.AUGUST));
+        System.out.println(seasonDuration.getSeason(Month.SEPTEMBER));
+        System.out.println(seasonDuration.getSeason(Month.OCTOBER));
+        System.out.println(seasonDuration.getSeason(Month.NOVEMBER));
+        System.out.println(seasonDuration.getSeason(Month.DECEMBER));
     }
 }
