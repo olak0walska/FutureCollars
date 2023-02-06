edf580d148b2f09582bc6ed7943f92d00dc8b52d
Aleksandra Kowalska <okowalska57@gmail.com>
2023-02-06 18:47:17 +0100

    .

--- src/main/java/org/example/lesson5task1/Main.java
+++ src/main/java/org/example/lesson5task1/Main.java
@@ -2,22 +2,37 @@ package org.example.lesson5task1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(isPalindrome("kajak"));
-        System.out.println(isPalindrome(""));
-        System.out.println(isPalindrome("qwerty"));
+
+        System.out.println(checkText("kajak"));
+        System.out.println(checkText(""));
+        System.out.println(checkText("qwerty"));
+
     }
 
-    static boolean isPalindrome(String text) {
-        if (text.length() > 0) {
-            System.out.println(text.length());
+    public static String checkText(String text) {
+        if (isNotEmpty(text)) {
+            System.out.println("Text is not empty.");
+            System.out.println("Text is a palindrome: " + isPalindrome(text));
+            System.out.println("Text length: " + getLength(text));
         } else {
-            return false;
+            System.out.println("Text is empty");
         }
+        return "";
+    }
 
+    private static boolean isNotEmpty(String text) {
+        if (text.length() > 0) ;
+        return true;
+    }
+
+    private static boolean isPalindrome(String text) {
         for (int i = 0; i < text.length(); i++)
             if (text.charAt(i) != text.charAt(text.length() - 1 - i))
                 return false;
         return true;
     }
 
+    private static int getLength(String text) {
+        return text.length();
+    }
 }
